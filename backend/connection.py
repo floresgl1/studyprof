f64e18a4f351311ddc533db14b0e07a9b55cddee
floresgl1 <floresgl907@gmail.com>
2026-08-29 21:56:45 -0700

    Add database tables and user creation endpoint in FastAPI

--- backend/connection.py
+++ backend/connection.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, ForeignKey, DateTime
 
 import os 
 from dotenv import load_dotenv
@@ -9,3 +9,44 @@ POSTGRES_PASS=os.getenv("POSTGRES_PASS")
 POSTGRES_USER=os.getenv("POSTGRES_USER")
 
 engine = create_engine(f"postgresql://{POSTGRES_USER}:{POSTGRES_PASS}@localhost/studyprof")
+
+if not POSTGRES_USER or not POSTGRES_PASS:
+    raise ValueError("POSTGRES_USER or POSTGRES_PASS is not set in the environment variables")
+
+
+metadata_obj = MetaData()
+
+
+users = Table(
+    "users",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("user_name", String, nullable=False),
+    Column("email", String, nullable=False)
+)
+
+subjects = Table(
+    "subjects",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("user_id", Integer, ForeignKey("users.id")),
+    Column("topic", String),
+    Column("sub_topic", String),
+    Column("short_description", String)
+)
+
+quizzes = Table(
+    "quizzes",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("user_id", Integer, ForeignKey("users.id")),
+    Column("subject_id", Integer, ForeignKey("subjects.id")),
+    Column("date", DateTime),
+    Column("proficiency", String),
+    Column("question", String),
+    Column("answer", String)
+)
+
+
+
+metadata_obj.create_all(engine)  # Create all tables defined in the metadata
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+#import tables from connection.py
 
+from connection import users, subjects, quizzes, engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import insert
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+# database setup
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+class UserCreate(BaseModel):
+    user_name: str
+    email:str
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/users/")
+async def receive_data(data: UserCreate, db: Session = Depends(get_db)):
+    
+    new_user = users.insert().values(user_name=data.user_name, email=data.email)
+    db.execute(new_user)
+    db.commit()
+    
+    return {
+        "status": "success",
+        "received_data": data,
+    }
+
+
+    
+    
+    
